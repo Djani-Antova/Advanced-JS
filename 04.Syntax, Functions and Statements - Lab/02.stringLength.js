5bc84857eb2deaae71f1696b0af4e23867e2e99a
djaniAntova <djaniantova@gmail.com>
2022-12-14 16:18:05 +0200

    Add task solutions
    Add task solutions for Lab: Syntax, Functions and Statements

---
+++ 04.Syntax, Functions and Statements - Lab/02.stringLength.js
@@ -0,0 +1,9 @@
+function stringLength(param1, param2, param3) {
+    let sum = param1.length + param2.length + param3.length;
+    let avg = Math.floor(sum / 3)
+
+    console.log(sum);
+    console.log(avg);
+    
+}
+stringLength ('pasta', '5', '22.3')
