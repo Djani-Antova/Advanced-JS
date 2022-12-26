9208ffb305b6c9bf5baa0d84ab3ccfcf5b506841
djaniAntova <djaniantova@gmail.com>
2022-12-26 11:16:41 +0200

    Add task solution

---
+++ 04.Syntax, Functions and Statements -Exercise/07.cookingByNumbersWIthFunctions.js
@@ -0,0 +1,57 @@
+function cookingByNumbers(num, p1, p2, p3, p4, p5) {
+
+    let inputAsNumber = Number(num);
+
+    let arrOfCommands = [p1, p2, p3, p4, p5];
+
+    let chop = function () {
+        return inputAsNumber / 2
+    }
+    let dice = function () {
+        return Math.sqrt(inputAsNumber)
+    }
+    let spice = function () {
+        return inputAsNumber + 1
+    }
+    let bake = function () {
+        return inputAsNumber * 3
+    }
+    let fillet = function () {
+        return inputAsNumber * 0.8
+    }
+
+    for (let i = 0; i < arrOfCommands.length; i++) {
+        let currentCommand = arrOfCommands[i];
+        switch (currentCommand) {
+            case 'chop':
+                inputAsNumber = chop(inputAsNumber);
+                console.log(inputAsNumber);
+                break;
+
+            case 'dice':
+                inputAsNumber = dice(inputAsNumber);
+                console.log(inputAsNumber);
+                break;
+
+            case 'spice':
+                inputAsNumber = spice(inputAsNumber);
+                console.log(inputAsNumber);
+                break;
+
+            case 'bake':
+                inputAsNumber - bake(inputAsNumber)
+                console.log(inputAsNumber);
+
+                break;
+            case 'fillet':
+                inputAsNumber = fillet(inputAsNumber)
+                console.log(inputAsNumber);
+
+                break;
+        }
+
+    }
+
+}
+cookingByNumbers('32', 'chop', 'chop', 'chop', 'chop', 'chop')
+//cookingByNumbers('9', 'dice', 'spice', 'chop', 'bake', 'fillet')
