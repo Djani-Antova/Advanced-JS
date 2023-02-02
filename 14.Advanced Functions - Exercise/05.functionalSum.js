410f2051fa037e65622f3c632aa01c14c87c073a
djaniAntova <djaniantova@gmail.com>
2023-02-02 12:15:57 +0200

    Add task solution

---
+++ 14.Advanced Functions - Exercise/05.functionalSum.js
@@ -0,0 +1,17 @@
+function add(num) {
+
+    let sum = 0;
+
+    function inner(number) {
+        sum += number;
+        return inner;
+    }
+
+    inner.toString = () => {
+        return sum;
+    }
+
+    return inner(num)
+}
+console.log(add(1)  (6)(-3).toString());
+// (add(1)(6)(-3))
