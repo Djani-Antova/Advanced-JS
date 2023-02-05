9a0f8ae8993c9a73acf3829d87ce34c57dd34ba3
djaniAntova <djaniantova@gmail.com>
2023-02-05 09:51:51 +0200

    Add test solution

---
+++ 16.Unit Testing and Error Handling -Exercise/03.lookUpChar.test.js
@@ -0,0 +1,33 @@
+const lookupChar = require('./index.js');
+const { assert } = require('chai');
+
+
+
+describe('Test the function of lookupChar', () => {
+
+    it('Return char at index', () => {
+        assert(lookupChar('Love', 1) === 'o');
+    });
+    it('Return char at index', () => {
+        assert(lookupChar('L', 0) === 'L');
+    });
+    it('Index over the string length', () => {
+        assert(lookupChar('Love', 10) === 'Incorrect index');
+    });
+    it('Negative string index', () => {
+        assert(lookupChar('Love', -10) === 'Incorrect index');
+    });
+    it('Empty string as input', () => {
+        assert(lookupChar('', 0) === 'Incorrect index');
+    });
+    it('Return undefined if first parameter is not string', () => {
+        assert(lookupChar(100, -10) === undefined);
+    });
+    it('Return undefined with string first parameter and decimal second', () => {
+        assert(lookupChar('Love', 10.5) === undefined);
+    });
+    it('Return undefined with wrong parameters type', () => {
+        assert(lookupChar(10, 'Love',) === undefined);
+    });
+
+});
