0866dd9f19f7bcfb52b0cf6a4cf5417285f1c7e8
djaniAntova <djaniantova@gmail.com>
2023-02-23 15:24:09 +0200

    Add task solution

--- 23.Former-Exams/Exam - 19 February 2022/01. Work Process/app.js
+++ 23.Former-Exams/Exam - 19 February 2022/01. Work Process/app.js
@@ -1,72 +1,112 @@
 function solve() {
-    const fnameInput = document.getElementById('fname');
-    const lnameInput = document.getElementById('lname');
+    const firstNameInput = document.getElementById('fname');
+    const lastNameInput = document.getElementById('lname');
     const emailInput = document.getElementById('email');
     const birthInput = document.getElementById('birth');
     const positionInput = document.getElementById('position');
     const salaryInput = document.getElementById('salary');
-    const table = document.querySelector('table')
-    const spanSum = document.querySelector('#sum')
-
-    const addWorkerBtn = document.getElementById('add-worker');
-    addWorkerBtn.addEventListener('click', add);
-
-    function add(event) {
-        event.preventDefault()
-
-        const fname = fnameInput.value;
-        const lname = lnameInput.value;
+ 
+    const hireWorkerButton = document.getElementById('add-worker');
+ 
+    hireWorkerButton.addEventListener('click', hireWorker);
+ 
+    function hireWorker(event) {
+        event.preventDefault();
+ 
+        const firstName = firstNameInput.value;
+        const lastName = lastNameInput.value;
         const email = emailInput.value;
         const birth = birthInput.value;
         const position = positionInput.value;
         const salary = salaryInput.value;
-
-        if (fname == '' || lname == '' || email == '' || birth == '' || position == '' || salary == '') {
-            return
+ 
+        if (
+            firstNameInput.value === '' ||
+            lastNameInput.value === '' ||
+            emailInput.value === '' ||
+            birthInput.value === '' ||
+            positionInput.value === '' ||
+            salaryInput.value === ''
+        ) {
+            return;
         }
-
-        const element = document.createElement('tbody');
-        element.innerHTML = `
-            <tbody id="tbody">
-                <tr>
-                    <td>${fname}</td>
-                    <td>${lname}</td>
-                    <td>${email}</td>
-                    <td>${birth}</td>
-                    <td>${position}</td>
-                    <td>${salary}</td>
-                    <td><button class='fired'>Fired</button> <button class="edit">Edit</button></td>
-                </tr>
-            </tbody>`
-
-        table.appendChild(element)
-        element.querySelector(' .fired').addEventListener('click', fired)
-        element.querySelector(' .edit').addEventListener('click', edit)
-
-        spanSum.textContent = (Number(spanSum.textContent) + Number(salary)).toFixed(2)
-
-        fnameInput.value = '';
-        lnameInput.value = '';
+ 
+        const tbody = document.getElementById('tbody');
+        const tr = document.createElement('tr');
+ 
+        const tdFirstName = document.createElement('td');
+        tdFirstName.textContent = firstNameInput.value;
+ 
+        const tdLastName = document.createElement('td');
+        tdLastName.textContent = lastNameInput.value;
+ 
+        const tdEmail = document.createElement('td');
+        tdEmail.textContent = emailInput.value;
+ 
+        const tdBirth = document.createElement('td');
+        tdBirth.textContent = birthInput.value;
+ 
+        const tdPosition = document.createElement('td');
+        tdPosition.textContent = positionInput.value;
+ 
+        const tdSalary = document.createElement('td');
+        tdSalary.classList.add('salary');
+        tdSalary.textContent = salaryInput.value;
+ 
+        const tdButtons = document.createElement('td');
+ 
+        const firedButton = document.createElement('button');
+        firedButton.classList.add('fired');
+        firedButton.textContent = 'Fired';
+ 
+        const editButton = document.createElement('button');
+        editButton.classList.add('edit');
+        editButton.textContent = 'Edit';
+ 
+        tdButtons.appendChild(firedButton);
+        tdButtons.appendChild(editButton);
+ 
+        tr.appendChild(tdFirstName);
+        tr.appendChild(tdLastName);
+        tr.appendChild(tdEmail);
+        tr.appendChild(tdBirth);
+        tr.appendChild(tdPosition);
+        tr.appendChild(tdSalary);
+        tr.appendChild(tdButtons);
+ 
+        tbody.appendChild(tr);
+ 
+        const spanSum = document.getElementById('sum');
+        // spanSum.textContent = (Array.from(tbody.querySelectorAll('.salary')).reduce((a, b) => a + Number(b.textContent), 0)).toFixed(2);
+        spanSum.textContent = (Number(tdSalary.textContent) + Number(spanSum.textContent)).toFixed(2);
+ 
+        firstNameInput.value = '';
+        lastNameInput.value = '';
         emailInput.value = '';
         birthInput.value = '';
         positionInput.value = '';
         salaryInput.value = '';
-
+ 
+        editButton.addEventListener('click', edit);
+ 
         function edit() {
-            fnameInput.value = fname;
-            lnameInput.value = lname;
+            tr.remove();
+ 
+            firstNameInput.value = firstName;
+            lastNameInput.value = lastName;
             emailInput.value = email;
             birthInput.value = birth;
             positionInput.value = position;
             salaryInput.value = salary;
-
-            element.remove()
-            spanSum.textContent = (Number(spanSum.textContent) - Number(salary)).toFixed(2)
+ 
+            spanSum.textContent = (Number(spanSum.textContent) - Number(salary)).toFixed(2);
         }
-
+ 
+        firedButton.addEventListener('click', fired);
+ 
         function fired() {
-            element.remove()
-            spanSum.textContent = (Number(spanSum.textContent) - Number(salary)).toFixed(2)
+            tr.remove();
+            spanSum.textContent = (Number(spanSum.textContent) - Number(salary)).toFixed(2);
         }
     }
 }
